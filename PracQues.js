6f40b5f8952edc45624da60b67975f3d6270b791
suhanaparvints <suhanaparvints@gmail.com>
2026-08-22 16:34:11 +0530

    Adding PracticeQuestion File

---
+++ PracQues.js
@@ -0,0 +1,20 @@
+//Print “Hello World”
+console.log("Hello World");
+
+//Add two numbers
+let  a=2, b=3;
+console.log(a + b);
+
+//Find even/odd numbers
+let num=2;
+if(num % 2 === 0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
+
+//Loop through an array
+let cars=["Volvo", "GTR", "BMW", "Toyota"];
+for(let i=0; i<cars.length; i++){
+    console.log(cars[i]);
+}
