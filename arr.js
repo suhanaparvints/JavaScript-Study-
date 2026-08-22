68924964a7fec34a743d4618150d77e6066e877f
suhanaparvints <suhanaparvints@gmail.com>
2026-08-22 18:01:44 +0530

    arr.js

--- arr.js
+++ arr.js
@@ -7,23 +7,15 @@ console.log(fruits1)
 
 //array can store different types of values
 let data = ["Suhana", 20, true, 10.5]
-
 console.log(data)
 
 // array index
  let fruits = ["Apple", "Banana", "Orange", "Mango"]
+console.log(fruits[0])// Apple
+console.log(fruits[1])// Banana
+console.log(fruits[2])// Orange
+console.log(fruits[3])// Mango
 
-console.log(fruits[0])
-// Apple
-
-console.log(fruits[1])
-// Banana
-
-console.log(fruits[2])
-// Orange
-
-console.log(fruits[3])
-// Mango
 // changing array element
 fruits[1] = "Grapes"
 console.log(fruits)
@@ -33,12 +25,12 @@ let numbers = [10, 20, 30, 40, 50]
 console.log(numbers.length) // 5
 console.log(numbers[numbers.length - 1]) // 50
 
-//adding element using push()
+//adding element using push() to end of array
 let fruits2 = ["Apple", "Banana"]
 fruits2.push("Orange")
 console.log(fruits2)
 
-//removing element using pop()
+//removing element using pop() at the end of array
 let fruits3 = ["Apple", "Banana", "Orange"]
 fruits3.pop()
 console.log(fruits3)
@@ -56,12 +48,9 @@ fruits5.shift()
 console.log(fruits5)
 
 //array checking using isArray()
-console.log(Array.isArray([1, 2, 3]))
-// true
-console.log(Array.isArray(10))
-// false
-console.log(Array.isArray("Suhana"))
-// false
+console.log(Array.isArray([1, 2, 3])) // true
+console.log(Array.isArray(10)) // false
+console.log(Array.isArray("Suhana")) // false
 
 //Objects Inside an Array
 let students = [
@@ -78,7 +67,5 @@ let students = [
         age: 22
     }
 ]
-console.log(students[0].name)
-// Suhana
-console.log(students[1].age)
-// 21
+console.log(students[0].name) // Suhana
+console.log(students[1].age) // 21
