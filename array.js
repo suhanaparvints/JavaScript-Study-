bec171f598118f248448c6e50dadacfe6bbc87aa
suhanaparvints <suhanaparvints@gmail.com>
2026-09-01 22:27:31 +0530

    array.js

--- array.js
+++ array.js
@@ -111,10 +111,6 @@ const fruits1 = ["Apple", "Banana", "Mango", "Carrot"];
 const result1 = fruits1.join(", ");
 console.log(result1); //Apple, Banana, Mango, Carrot
 
-//! at() - Gets an element by index.
-console.log(fruits1.at(2)); //mango
-console.log(fruits1.at(-1));  //! negative indexing
-
 //! fill() - method replaces all or selected elements of an array with a specified value.
 //fill(value, start, end) [value - means changing value]
 const numbers1 = [1, 2, 3, 4, 5];
@@ -179,34 +175,7 @@ console.log(no);
 no.copyWithin(-2,0,2); //negative indexes
 console.log(no);
 
-//! flat()
-// flat() is an array method used to remove nested array levels and create a flatter array. 
-// ⭐ Interview definition
-// flat() creates a new array by recursively concatenating elements from nested arrays up to a specified depth.By default, it flattens one level and does not modify the original array.
-const numbers0 = [6, 7, [8, 9], 10];
-const result0 = numbers0.flat();  //[6,7,8,9,10]]
-console.log(result0);
-
-//flat() removes one level of nesting.
-//Multiple Nested Arrays
-const numbers9 = [1, [2, [3, 4]]];
-const result9 = numbers9.flat();
-console.log(result9);
-// flat(2) ⭐
-// To flatten two levels:
-console.log(numbers9.flat(2));
-//flat(Infinity) ⭐
-const numbers8 = [1, [2, [3, [4, [5]]]]];
-console.log(numbers8.flat(Infinity));
-//flat() with Empty Slots - removes empty slots
-const numbers7 = [1, , 3];
-console.log(numbers7.flat());
-//flat() with string
-const data1 = ["Hello", ["JavaScript", "World"]];
-console.log(data1.flat());
-
-
-//Advanced JS
+//Advanced Array Operation in JS
 //! sort() - sorts the elements of an array in place and returns the sorted array. By default, it sorts elements as strings in ascending order.sort() Changes the Original Array
 //used to arrange elements of an array in a particular order
 // Sorting Strings
@@ -288,3 +257,164 @@ const numbers4 = [10, 20, 30, 40];
 console.log(numbers4.at(-1)); // at(-1) only gets the last element. It doesn't reverse the array.
 
 
+
+//! flat()
+// flat() is an array method used to remove nested array levels and create a flatter array. 
+// ⭐ Interview definition
+// flat() creates a new array by recursively concatenating elements from nested arrays up to a specified depth.By default, it flattens one level and does not modify the original array.
+const numbers0 = [6, 7, [8, 9], 10];
+const result0 = numbers0.flat();  //[6,7,8,9,10]]
+console.log(result0);
+
+//flat() removes one level of nesting.
+//Multiple Nested Arrays
+const numbers9 = [1, [2, [3, 4]]];
+const result9 = numbers9.flat();
+console.log(result9);
+// flat(2) ⭐
+// To flatten two levels:
+console.log(numbers9.flat(2));
+//flat(Infinity) ⭐
+const numbers8 = [1, [2, [3, [4, [5]]]]];
+console.log(numbers8.flat(Infinity));
+//flat() with Empty Slots - removes empty slots
+const numbers7 = [1, , 3];
+console.log(numbers7.flat());
+//flat() with string
+const data1 = ["Hello", ["JavaScript", "World"]];
+console.log(data1.flat());
+
+//! at() - Gets an element by index.
+console.log(fruits1.at(2)); //mango
+console.log(fruits1.at(-1));  //! negative indexing
+
+//! flatMap()
+
+/* Functional JavaScript ⭐⭐⭐
+forEach()
+map()
+filter()
+find()
+some()
+every()
+reduce()
+These are higher-order array methods because they accept callback functions. */
+
+//! forEach()
+// forEach() executes a function once for each element.
+const nums=[10,20,30,40];
+nums.forEach(function(numbers){
+    console.log(numbers);
+});
+//Arrow Function in forEach()
+nums.forEach(numbers =>{
+console.log(numbers);
+});
+//forEach() with index and value
+nums.forEach((num,index)=>{
+    console.log(index, num); //(index, value)
+});
+//forEach() in Callback Parameter return index,value and array
+nums.forEach((num,index,nums)=>{
+console.log(index,num,nums);  //(index,value, array)
+});
+
+//! map() - Transform every element ⭐⭐⭐
+// creates a new array by changing each element.
+const books = new Map([["javascript", "JS"], ["html", "HTML"], ["css", "CSS"]]);
+books.set("java", "JAVA");  //adding value using Map.set()
+console.log(books);
+books.set("java", "Robust") // changing value using Map.set()
+console.log(books);
+books.get("html");  // get value of key using Map.get()
+console.log(books);
+console.log(typeof(books));  // typeof Map object
+//instanceof Map //true
+books.delete("java"); //delete key value using Map.delete()
+console.log(books);
+// books.has("javascript"); // return true/false when the key exist
+// console.log(books);
+books.clear();  // remove all elements using Map.clear()
+console.log(books);
+
+//! Object as Keys
+const apples={name:"Apple"};
+const bananas={name:"Bananas"};
+const orange={name:"Orange"};
+//create a Map
+const fruites=new Map();
+//add new elements to map
+fruites.set(apples, 100);
+fruites.set(bananas, 300);
+fruites.set(orange, 500);
+//output
+console.log(apples);
+console.log(bananas);
+console.log(orange);
+console.log(fruites);
+//Remember the key is an object(apples) not strings("apples")
+
+// Real Example of Map
+//doesn't modify the original array.
+const number2=[10,9,8,7,6,5];
+const doubled=number2.map(num=>{
+    return num*2;
+});
+console.log(doubled); // [20,18,16,14,12,10]
+
+//! filter() - Select elements ⭐⭐⭐
+//creates a new array containing only the elements that satisfy a condition.
+const newNums=[1,2,3,4,5,6];
+const even=newNums.filter(num=>num%2==0);
+console.log(even);  //[2,4,6]
+// Example with objects of filter()
+const students0=[
+    {name:"suhana", age:20},
+    {name:"parvin", age:14},
+    {name:"suhu", age:20.5}
+];
+const adult=students0.filter(students0 => students0.age>=18);
+console.log(adult);
+
+//!reduce() — Reduce to one value ⭐⭐⭐
+//processes all elements and produces one final value.
+// accumulate values into any data type, including numbers, strings, booleans, objects, and arrays
+// Syntax: array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+//currentIndex, arr, initialValue are optionals
+const nameu=[10,20,30,40];
+const sum=nameu.reduce((total,num)=>{
+    return total+num;
+}, 0);
+console.log(sum);   //100
+
+// reduce() in array object
+const products = [
+    { name: "Laptop", price: 50000 },
+    { name: "Phone", price: 20000 },
+    { name: "Mouse", price: 1000 },
+    { name: "Keyboard", price: 2000 }
+];
+const totals = products.reduce((sum, product) => {
+    return sum + product.price;
+}, 0);
+console.log(totals);    //73000
+
+//Chaining ⭐⭐⭐⭐
+//you'll start using them together.
+const numbers00 = [1, 2, 3, 4, 5, 6];
+const results00 = numbers00.filter(num => num % 2 === 0).map(num => num * 10);
+console.log(results00);  //[20,40,60]
+
+//! some() - Check at least one matches condition and return boolean(true / false)
+const numb=[1,3,4,8];
+const results1=numb.some(num => num%2===0);
+console.log(results1);    //true
+
+//! every() - Check all elements that matches the condition and returns boolean(true / false)
+const numr=[1,3,4,5,8,10];
+const results2 =numr.every(num => num%2===0);
+console.log(results2);  // false 
+
+const numrs=[2,4,6,8];
+const results12 =numrs.every(num => num%2===0);
+console.log(results12);  // true
